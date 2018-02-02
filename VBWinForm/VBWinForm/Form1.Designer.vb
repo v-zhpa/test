34bf2f7069afb04ce6e6d64c51cc8e48521c37cf
v-zhpa <v-zhpa@microsoft.com>
2018-02-01 22:15:45 -0800

    Add some code about this demo_20180202

--- VBWinForm/VBWinForm/Form1.Designer.vb
+++ VBWinForm/VBWinForm/Form1.Designer.vb
@@ -30,12 +30,19 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(120, 106)
+        Me.Label1.Location = New System.Drawing.Point(127, 112)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(14, 13)
         Me.Label1.TabIndex = 0
@@ -44,7 +51,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(120, 149)
+        Me.Label2.Location = New System.Drawing.Point(127, 149)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(14, 13)
         Me.Label2.TabIndex = 1
@@ -67,7 +74,7 @@ Partial Class Form1
         'Button1
         '
         Me.Button1.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Button1.Location = New System.Drawing.Point(155, 205)
+        Me.Button1.Location = New System.Drawing.Point(361, 207)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(57, 25)
         Me.Button1.TabIndex = 4
@@ -78,36 +85,111 @@ Partial Class Form1
         '
         Me.Button2.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.Button2.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Button2.Location = New System.Drawing.Point(261, 205)
+        Me.Button2.Location = New System.Drawing.Point(185, 207)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(57, 25)
         Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Sum"
+        Me.Button2.Text = "+"
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(127, 173)
+        Me.Label3.Location = New System.Drawing.Point(127, 185)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
         Me.Label3.TabIndex = 6
-        Me.Label3.Text = "result"
+        Me.Label3.Text = "Result"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(238, 173)
+        Me.Label4.Location = New System.Drawing.Point(120, 64)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(30, 13)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "Time"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(361, 57)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(57, 27)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "TimNow"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(127, 301)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 9
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(185, 314)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(142, 28)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Save Result"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(185, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(141, 27)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Calculatur"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button6.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Button6.Location = New System.Drawing.Point(270, 207)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(57, 25)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "-"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button7.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Button7.Location = New System.Drawing.Point(185, 251)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(57, 25)
+        Me.Button7.TabIndex = 13
+        Me.Button7.Text = "X"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button8.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Button8.Location = New System.Drawing.Point(270, 251)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(57, 25)
+        Me.Button8.TabIndex = 14
+        Me.Button8.Text = "/"
+        Me.Button8.UseVisualStyleBackColor = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button2)
@@ -131,4 +213,11 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
 End Class
--- VBWinForm/VBWinForm/Form1.vb
+++ VBWinForm/VBWinForm/Form1.vb
@@ -5,7 +5,7 @@
         TextBox2.Text = ""
         Label3.Text = ""
         Label4.Text = ""
-
+        Label5.Text = ""
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -18,12 +18,68 @@
         Label3.Text = X & "+" & Y & "=" & S
 
 
+    End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Label4.Text = DateTime.Now.ToString()
-        Application.DoEvents()
-        System.Threading.Thread.Sleep(100)
+    End Sub
 
 
+    Private Sub PrintOnLabel(ByVal x)
+        Dim a As String
+        '转换成字符串
+        a = Trim(CStr(x))
+        '如果字符串为空，则不显示并退出Sub
+        If a = "" Then Exit Sub
+        '在Label1上显示数字！注意：这里使用了拼接字符串以及换行符vbCrLf
+        Label5.Text = Label5.Text & CStr(x) & vbCrLf
+        'Form_Load()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim a As String
+        a = Label3.Text
+        PrintOnLabel(a)
+
+    End Sub
+
+    Private Sub Form_Load()
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        MessageBox.Show("Hello, Welcome to use Calculator!")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim X As Integer
+        Dim Y As Integer
+        Dim S As Integer
+        X = Val(TextBox1.Text)
+        Y = Val(TextBox2.Text)
+        S = X - Y
+        Label3.Text = X & "-" & Y & "=" & S
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim X As Integer
+        Dim Y As Integer
+        Dim S As Integer
+        X = Val(TextBox1.Text)
+        Y = Val(TextBox2.Text)
+        S = X * Y
+        Label3.Text = X & "*" & Y & "=" & S
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim X As Integer
+        Dim Y As Integer
+        Dim S As Integer
+        X = Val(TextBox1.Text)
+        Y = Val(TextBox2.Text)
+        S = X / Y
+        Label3.Text = X & "/" & Y & "=" & S
     End Sub
 
 End Class
